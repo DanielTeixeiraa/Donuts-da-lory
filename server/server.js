b04de48ae2b1da0e42f7e741e807a6278a5e18e0
Daniel <Daniel.teixeirari@gmail.com>
2020-06-06 23:40:46 -0300

    Fix responsividade

--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
-const PORT = process.env.PORT ||  5000;  
+const express = require("express");    
+const nunjucks = require("nunjucks");       
+const PORT = process.env.PORT ||  5000;     
    
 const app = express(); 
-const data = require("./data");  
+const data = require("./data");   
  
-app.use(express.static("../views/public"));
-
-app.set("view engine", "njk");
- 
-nunjucks.configure("../views",{ 
+app.use(express.static("../views/public")); 
+   
+app.set("view engine", "njk");    
+   
+nunjucks.configure("../views",{      
   express: app,
-  autoescape:false 
+  autoescape:false  
 });
 
 app.get("/", (req,res) =>{
   return res.render("inicio",{data:data})
-}) 
+})
+
+app.get('/sobre', (req,res) =>{
+  return res.render('sobre')
+})
 
 app.listen(PORT, () =>{
   console.log("server running")
